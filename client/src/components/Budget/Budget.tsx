b69800eb132c11580bc45b37ce08be68bd966e0c
kishenrex <kishenthevan@gmail.com>
2024-10-28 15:52:31 -0700

    finished ex 2

--- client/src/components/Budget/Budget.tsx
+++ client/src/components/Budget/Budget.tsx
@@ -1,9 +1,26 @@
 import { AppContext } from "../../context/AppContext";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { fetchBudget } from "../../utils/budget-utils";
 
 const Budget = () => {
 
-  const { budget } = useContext(AppContext);
+  const { budget, setBudget } = useContext(AppContext);
+
+  // Fetch budget on component mount
+  useEffect(() => {
+    loadBudget();
+    }, []);
+  
+    // Function to load expenses and handle errors
+    const loadBudget = async () => {
+    try {
+          const budgetAmount = await fetchBudget();
+          console.log("budget amount is" + budgetAmount)
+          setBudget(budgetAmount);
+    } catch (err: any) {
+          console.log(err.message);
+    }
+    };
 
   return (
     <div className="alert alert-secondary p-3 d-flex align-items-center justify-content-between">
